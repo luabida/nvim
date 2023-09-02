bf3188e41c3c8951e6545726fdf5d0c696720576
Luã Bida Vacaro <luabidaa@gmail.com>
2023-09-02 18:14:39 -0300

    Replace python lsp for a better server & config

--- lua/utils.lua
+++ lua/utils.lua
@@ -4,7 +4,9 @@ local f_mod = vim.fn.fnamemodify
 local f_esc = vim.fn.fnameescape
 local pshorten = vim.fn.pathshorten
 
-local function cap_path_length(path)
+local M = {}
+
+function M.cap_path_length(path)
   if string.len(path) > 50 then
     path = pshorten(path)
   end
@@ -12,15 +14,20 @@ local function cap_path_length(path)
   return path
 end
 
-local function display_path(path)
+function M.display_path(path)
   local absolute_path = f_mod(path, ':p')
   local escaped_path = f_esc(absolute_path)
   return get_icon(escaped_path, f_mod(escaped_path, ':e'), { default = true })
     .. ' '
-    .. cap_path_length(f_mod(absolute_path, ':~:.'))
+    .. M.cap_path_length(f_mod(absolute_path, ':~:.'))
 end
 
-return {
-  cap_path_length = cap_path_length,
-  display_path = display_path,
-}
+function M.executable(name)
+  if vim.fn.executable(name) > 0 then
+    return true
+  end
+
+  return false
+end
+
+return M
